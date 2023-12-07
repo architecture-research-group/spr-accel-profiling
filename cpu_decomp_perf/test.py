25f0b4cab8887fa395d73059c0d4d2b64b2db123
Neel Patel <n869p538@sapphire.ittc.ku.edu>
2023-12-07 13:12:30 -0600

    refactor for more compressors

--- cpu_decomp_perf/test.py
+++ cpu_decomp_perf/test.py
@@ -14,30 +14,37 @@ TARGETS = [
 		'name': 'zlib',
 		'library': '-lz',
 		'cflags': [ ],
+		'tests': glob.glob('corpus/*.zlib6')
+	},
+	{
+		'name': 'zstd',
+		'library': '-lzstd',
+		'cflags': [ ],
+		'tests': glob.glob('corpus/*.zst1')
 	},
 ]
 
-TESTS = glob.glob('corpus/*.zlib6')
-TESTS.sort(key=os.path.getsize)
+
 
 for target in TARGETS:
-	subprocess.run([CC] + target.get('cflags', []) + ['harness.cpp', target['library'], '-o', 'harness-' + target['name']], check=True)
+	subprocess.run([CC] + target.get('cflags', []) + ['harness-' + target['name'] + '.cpp', target['library'], '-o', 'harness-' + target['name']], check=True)
+	TESTS = target['tests']
+	TESTS.sort(key=os.path.getsize)
 
+	for test in TESTS:
 
-for test in TESTS:
+		last = None
 
-	last = None
+		runs = 100
 
-	runs = 100
+		results = []
+		for target in TARGETS:
 
-	results = []
-	for target in TARGETS:
-
-		r = subprocess.check_output(['./harness-' + target['name'], test, '1', str(runs)])
-		print(r)
-		# results.append('%.1f' % (r,))
-		# if last is not None:
-		# 	results[-1] += (' (%.3fx)' % (r / last,))
-		# last = r
+			r = str(subprocess.check_output(['./harness-' + target['name'], test, '1', str(runs)]))
+			print(test+','+r)
+			# results.append('%.1f' % (r,))
+			# if last is not None:
+			# 	results[-1] += (' (%.3fx)' % (r / last,))
+			# last = r
 
 	print('\t'.join([test.split('/')[-1]] + results))
--- cpu_decomp_perf/zlib_compress.py
+++ cpu_decomp_perf/zlib_compress.py
@@ -1,10 +1,21 @@
 import zlib
+import zstd
+import os
 
-def compress_file(infile):
+def zlib_compress_file(infile):
     level=6
     outfile = infile + '.zlib' + str(level)
     with open('rand.txt', 'rb') as f:
         data = f.read()
         comp = zlib.compress(data)
+        with open(outfile,'wb') as of:
+            of.write(comp)
+
+def zstd_compress_file(infile):
+    level=1
+    outfile = './corpus/' + os.path.basename(infile) + '.zst' + str(level)
+    with open(infile, 'rb') as f:
+        data = f.read()
+        comp = zstd.ZSTD_compress(data)
         with open(outfile,'wb') as of:
             of.write(comp)
