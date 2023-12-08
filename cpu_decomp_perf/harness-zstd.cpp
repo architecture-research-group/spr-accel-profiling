cb42b50a4592443ed02a2e5d8536a321a40ed91a
Neel Patel <n869p538@sapphire.ittc.ku.edu>
2023-12-08 06:23:16 -0600

    lz4 compress

---
+++ cpu_decomp_perf/harness-zstd.cpp
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <chrono>
+#include <algorithm>
+#include <numeric>
+
+#ifdef USE_LIBDEFLATE
+#include "libdeflate/libdeflate.h"
+#else
+#include <zstd.h>
+#endif
+
+char input_buffer[64 * 1024 * 1024];
+char output_buffer[64 * 1024 * 1024];
+
+uint64_t nano() {
+   return std::chrono::duration_cast< ::std::chrono::nanoseconds>(
+          std::chrono::steady_clock::now().time_since_epoch())
+          .count();
+}
+
+
+int main(int argc, char **argv) {
+
+   FILE *f = fopen(argv[1], "rb");
+
+   int iterations_per_run = atoi(argv[2]);
+   int runs = atoi(argv[3]), ret, size;
+   int compressed_size = fread(input_buffer, 1, sizeof input_buffer, f);
+
+   size_t outlen = sizeof output_buffer;
+   int status = ZSTD_decompress((uint8_t*)output_buffer, outlen, (uint8_t*)input_buffer, compressed_size);
+
+   double times[10000];
+
+   for (int j = 0; j < runs; j++) {
+      {
+         uint64_t start = nano();
+         for (int i = 0; i < iterations_per_run; i++) {
+            outlen = sizeof output_buffer;
+            size = ZSTD_decompress((uint8_t*)output_buffer, outlen, (uint8_t*)input_buffer, compressed_size);
+         }
+         uint64_t end = nano();
+         double bb = (end-start);
+         times[j] = bb/iterations_per_run;
+        //  printf("%lfns\n", times[j]);
+         double b = (end-start) / (double)(compressed_size*iterations_per_run) * 3.2;
+      }
+   }
+
+   std::sort(&times[0], &times[runs]);
+   double mean = std::accumulate(&times[0], &times[runs], 0)/runs;
+//    printf("%s-decomp(ns),median:%lf,mean:%lf,max:%lf,min:%lf\n", argv[1],times[runs/2], mean, times[runs-2], times[0]);
+   printf("%lf,%lf,%lf,%lf\n",size / compressed_size, [runs/2], mean, times[runs-2], times[0]);
+
+//    printf("%lf\n", compressed_size * iterations_per_run / (times[0] / 1000.0));
+   
+   return 0;
+}
--- cpu_decomp_perf/zlib_compress.py
+++ cpu_decomp_perf/zlib_compress.py
@@ -1,8 +1,7 @@
-import zlib
-import zstd
 import os
 
 def zlib_compress_file(infile):
+    import zlib
     level=6
     outfile = infile + '.zlib' + str(level)
     with open('rand.txt', 'rb') as f:
@@ -12,10 +11,21 @@ def zlib_compress_file(infile):
             of.write(comp)
 
 def zstd_compress_file(infile):
+    import zstd
     level=1
     outfile = './corpus/' + os.path.basename(infile) + '.zst' + str(level)
     with open(infile, 'rb') as f:
         data = f.read()
         comp = zstd.ZSTD_compress(data)
+        with open(outfile,'wb') as of:
+            of.write(comp)
+
+def lz4_compress_file(infile):
+    import lz4.frame
+    # level=1
+    outfile = './corpus/' + os.path.basename(infile) + '.lz4' #+ str(level)
+    with open(infile, 'rb') as f:
+        data = f.read()
+        comp = lz4.frame.compress(data)
         with open(outfile,'wb') as of:
             of.write(comp)
