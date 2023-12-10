20b4466cbb44660adaf2708f59dcab91cc29b20b
Neel Patel <n869p538@emerald.ittc.ku.edu>
2023-12-10 14:30:55 -0600

    fix bpftrace w/ debug syms

---
+++ cpu_decomp_perf/zswap_overhead/issues/source_debug_apt.sh
@@ -0,0 +1,3 @@
+echo "deb http://ddebs.ubuntu.com $(lsb_release -cs) main restricted universe multiverse
+deb http://ddebs.ubuntu.com $(lsb_release -cs)-updates main restricted universe multiverse
+deb http://ddebs.ubuntu.com $(lsb_release -cs)-proposed main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list.d/ddebs.list
