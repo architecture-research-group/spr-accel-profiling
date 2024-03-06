ee0875d72b8417712b0ea32bb405ce3ce4e52c93
Neel Patel <n869p538@sapphire.ittc.ku.edu>
2024-03-05 21:45:51 -0600

    discard first 10

--- cpu_compression_latency/harness-isal_zlib_decompress.cpp
+++ cpu_compression_latency/harness-isal_zlib_decompress.cpp
@@ -10,6 +10,8 @@
 #include "igzip_lib.h"
 #include <zlib.h>
 
+#define CALGARY "/lib/firmware/calgary"
+
 char input_buffer[64 * 1024 * 1024];
 char output_buffer[64 * 1024 * 1024];
 char output_buffer_2[64 * 1024 * 1024];
@@ -35,6 +37,37 @@ void flush_buf(char *buf, int size) {
    for (int i = 0; i < size; i+=64) {
       _mm_clflush(&buf[i]);
    }
+}
+
+void generate_list_of_buffers(char *input_buffer, int size, int num_buffers, char **buffers) {
+   for (int i = 0; i < num_buffers; i++) {
+      buffers[i] = (char *)malloc(size / num_buffers);
+      memcpy(buffers[i], input_buffer + (i * (size / num_buffers)), size/num_buffers);
+   }
+}
+
+/*
+ * This function reads the calgary corpus into a buffer and then splits it into
+   * num_buffers sub-buffers. It returns the number of sub-buffers created.
+*/
+int corpus_to_input_buffer(char **testBufs, int subBufSize ) {
+   FILE *f = fopen(CALGARY, "rb");
+   int size = fseek(f, 0, SEEK_END);
+   fseek(f, 0, SEEK_SET);
+
+   int *input_buffer = (int *)malloc(size);
+   int orig_size = fread(input_buffer, 1, size, f);
+   
+   int num_buffers = (size % num_buffers) == 0 ? (size / subBufSize) : (size / subBufSize) + 1;
+
+   char *buffer = (char *)malloc(num_buffers * subBufSize);
+   memcpy(buffer, input_buffer, orig_size);
+   if(orig_size < num_buffers * subBufSize) {
+      memcpy(buffer + orig_size, input_buffer, num_buffers * subBufSize - orig_size);
+   }
+
+   generate_list_of_buffers(buffer, orig_size, num_buffers, testBufs);
+   return num_buffers;
 }
 
 
@@ -82,6 +115,7 @@ int main(int argc, char **argv) {
    else
       runs = 10;
    iterations_per_run=1;
+   int discard_fst = 10;
    uint64_t latency_avg, latency_min, latency_max;
    vector<uint64_t> latencies(runs);
    for(int i=0; i<runs; i++){
@@ -100,13 +134,13 @@ int main(int argc, char **argv) {
       uint64_t end = nanos();
       printf("latency,%ld\n", end-start);
       assert(compare_buffers(output_buffer_2, input_buffer, size) == -1);
-      latencies.push_back((end - start)/iterations_per_run);      
+      latencies[i]=((end - start)/iterations_per_run);      
    }
    uint64_t max_latency = *max_element(&(latencies[0]), &(latencies[runs-1]));
    printf("ISAL-Decompress,%d,%f,%ld\n",
    size, 
-   std::accumulate(latencies.begin(),
-      latencies.end(), 0) / (runs*1.0),
+   std::accumulate(latencies.begin()+discard_fst,
+      std::next(latencies.begin(),runs-discard_fst), 0) / (runs*1.0),
       max_latency); 
 
    
@@ -131,14 +165,14 @@ int main(int argc, char **argv) {
       uint64_t end = nanos();
       printf("latency,%ld\n", end-start);
       assert(compare_buffers(output_buffer, backup_buffer, stream.total_out) == -1);
-      latencies.push_back(end - start);      
+      latencies[i] = (end - start)/iterations_per_run;      
    }
    max_latency = *max_element(&(latencies[0]), &(latencies[runs-1]));
    printf("ISAL-Compress,%d,%f,%f,%ld\n",
    size,
    size*1.0/stream.total_out,
-   std::accumulate(latencies.begin(),
-      latencies.end(), 0) / (runs*1.0),
+   std::accumulate(latencies.begin()+discard_fst,
+      std::next(latencies.begin(),runs-discard_fst), 0) / (runs*1.0),
       max_latency);   
 
    /* Zlib */
@@ -155,6 +189,7 @@ int main(int argc, char **argv) {
       if(do_flush){
          flush_buf(output_buffer, sizeof output_buffer);
          flush_buf(input_buffer, sizeof input_buffer);
+         _mm_mfence();
       }
       uint64_t start = nanos();
       for(int j=0; j<iterations_per_run; j++){
@@ -166,16 +201,19 @@ int main(int argc, char **argv) {
       }
       uint64_t end = nanos();
       // assert(compare_buffers(output_buffer, backup_buffer, size) == -1);
-      latencies.push_back((end - start)/iterations_per_run);    
+      latencies[i] = ((end - start)/iterations_per_run);    
       printf("latency,%ld\n", end-start);  
    }
+
    max_latency = *max_element(&(latencies[0]), &(latencies[runs-1]));
    printf("Zlib-Compress,%d,%f,%f,%ld\n",
    size,
    size*1.0/strm.total_out,
-   std::accumulate(latencies.begin(),
-      latencies.end(), 0) / (runs*1.0),
+   std::accumulate(latencies.begin()+discard_fst,
+      std::next(latencies.begin(),runs-discard_fst), 0) / (runs*1.0),
    max_latency);
+
+   printf("%lu,%lu\n", *(latencies.begin()+2), *(latencies.end()-2));
    
    return 0;
 }
