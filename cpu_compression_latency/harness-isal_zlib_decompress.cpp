7be96e2c247dd303c972342f99c1b7daae2d0591
Neel Patel <n869p538@sapphire.ittc.ku.edu>
2024-03-06 10:41:53 -0600

    decompression using isal giving mismatch

--- cpu_compression_latency/harness-isal_zlib_decompress.cpp
+++ cpu_compression_latency/harness-isal_zlib_decompress.cpp
@@ -6,6 +6,8 @@
 #include <cassert>
 #include <cstring>
 #include <immintrin.h>
+#include <fstream>
+#include <vector>
 
 #include "igzip_lib.h"
 #include <zlib.h>
@@ -45,6 +47,9 @@ void flush_buf(char *buf, int size) {
    }
 }
 
+/*
+ * populate list of buffers with sub-buffers of size size/num_buffers using contents of input_buffer
+*/
 void generate_list_of_buffers(char *input_buffer, int size, int num_buffers, char **buffers) {
    for (int i = 0; i < num_buffers; i++) {
       buffers[i] = (char *)malloc(size / num_buffers);
@@ -56,45 +61,39 @@ void generate_list_of_buffers(char *input_buffer, int size, int num_buffers, cha
  * This function reads the calgary corpus into a buffer and then splits it into
    * num_buffers sub-buffers. It returns the number of sub-buffers created.
 */
-int corpus_to_input_buffer(char ** &testBufs, int subBufSize ) {
-   FILE *f = fopen(CALGARY, "rb");
-   if (f == NULL) {
-      printf("Error: could not open file %s\n", CALGARY);
-      return -1;
+int corpus_to_input_buffer(char ** &testBufs,int sizePerBuf ) {
+   FILE *file = fopen(CALGARY, "rb");
+   fseek(file, 0, SEEK_END);
+   int size = ftell(file);
+   fseek(file, 0, SEEK_SET);
+   int max_bufs = size / sizePerBuf;
+   testBufs = (char **)malloc(sizeof(char *) * max_bufs);
+   std::ifstream infile(CALGARY, std::ios::binary);
+   int i=0;
+   if (file) {
+      while (true) {
+         testBufs[i] = (char *)malloc(sizePerBuf);
+         if (!infile.read(testBufs[i], sizePerBuf)) { /*fail if not enough content*/
+               break;
+         }
+         i++;
+      }
    }
-   fseek(f, 0, SEEK_END);
-   int size = ftell(f);
-   fseek(f, 0, SEEK_SET);
-
-   int *input_buffer = (int *)malloc(size);
-   int orig_size = fread(input_buffer, 1, size, f);
-   
-   int num_buffers = (size % num_buffers) == 0 ? (size / subBufSize) : (size / subBufSize) + 1;
-   testBufs = (char **)malloc(sizeof(char *) * num_buffers);
-   char *buffer = (char *)malloc(num_buffers * subBufSize);
-   memcpy(buffer, input_buffer, orig_size);
-   if(orig_size < num_buffers * subBufSize) {
-      memcpy(buffer + orig_size, input_buffer, (num_buffers * subBufSize) - orig_size);
-   }
-
-   generate_list_of_buffers(buffer, subBufSize*num_buffers, num_buffers, testBufs);
-   return num_buffers;
+   return i;
 }
 
 
 int main(int argc, char **argv) {
 
 
-   int iterations_per_run = 10;
    int size = atoi(argv[1]);
    int do_flush = atoi(argv[2]), ret;
+   int iterations_per_run = atoi(argv[3]);
+   int discard_fst = atoi(argv[4]);
 
 
-   char **test_bufs;
-   int num_bufs = corpus_to_input_buffer(test_bufs, size);
-
-
-   char **input_buffers = test_bufs;
+   char **input_buffers;
+   int num_bufs = corpus_to_input_buffer(input_buffers, size);
    char **output_buffers = (char **)malloc(sizeof(char *) * num_bufs);
    uint32_t compressed_sizes[num_bufs];
    char **output_buffers_2 = (char **)malloc(sizeof(char *) * num_bufs);
@@ -111,9 +110,8 @@ int main(int argc, char **argv) {
    isal_deflate_init(&stream);
 
    for(int i=0; i< num_bufs; i++){
-      uint8_t *input_buffer = (uint8_t *)input_buffers[i];
       backup_buffers[i] = (char *)malloc(size);
-      stream.next_in = input_buffer;
+      stream.next_in = (uint8_t *)input_buffers[i];
       stream.avail_in = size;
       stream.next_out = (uint8_t *)output_buffers[i];
       stream.avail_out = size;
@@ -121,68 +119,59 @@ int main(int argc, char **argv) {
       stream.flush = SYNC_FLUSH;
       isal_deflate(&stream);
       compressed_sizes[i] = stream.total_out;
+      backup_buffers[i] = (char *)malloc(stream.total_out);
       memcpy(backup_buffers[i], output_buffers[i], stream.total_out);
-      // printf("Compressed buffer %d   to %d\n", i, stream.total_out);
+      printf("Compressed buffer %d   to %d\n", i, stream.total_out);
       stream.total_out = 0;
    }
 
-   int runs;
-   int discard_fst = 10;
-   // if(!do_flush){
-      runs = 1000;
-      discard_fst = 100;
-   // }
-   // else{
-      // runs = 10;
-      // discard_fst = 1;
-   // }
    iterations_per_run=1;
    uint64_t latency_avg, latency_min, max_latency;
-   vector<uint64_t> latencies(runs);
+   vector<uint64_t> latencies(num_bufs);
 
    /* INFLATE */
    struct inflate_state state;
-   for(int i=0; i<runs; i++){      
+   for(int i=0; i<num_bufs; i++){      
       if(do_flush){
-         flush_buf(output_buffers[i%num_bufs], compressed_sizes[i%num_bufs]);
-         flush_buf(output_buffers_2[i%num_bufs], size);
+         flush_buf(output_buffers[i], compressed_sizes[i]);
+         flush_buf(output_buffers_2[i], size);
       }
       uint64_t start = micros();
       for(int j=0; j<iterations_per_run; j++){
-         state.next_in = (uint8_t*)output_buffers[i%num_bufs];
-         state.avail_in = compressed_sizes[i%num_bufs];
-         state.next_out = (uint8_t*)output_buffers_2[i%num_bufs];
+         state.next_in = (uint8_t*)output_buffers[i];
+         state.avail_in = compressed_sizes[i];
+         state.next_out = (uint8_t*)output_buffers_2[i];
          state.avail_out = size;
          isal_inflate(&state);
       }
       uint64_t end = micros();
       // printf("latency,%ld\n", end-start);
       int mismatch;
-      if( (mismatch = compare_buffers(output_buffers_2[i%num_bufs], input_buffers[i%num_bufs], size)) != -1){
-         printf("Error: Decompression mismatch buffer:%d index:%d \n", i%num_bufs, mismatch );
+      if( (mismatch = compare_buffers(output_buffers_2[i], input_buffers[i], size)) != -1){
+         printf("Error: Decompression mismatch buffer:%d index:%d \n", i, mismatch );
          // return -1;
       }
       latencies[i]=((end - start)/iterations_per_run);      
    }
-   max_latency  = *max_element(&(latencies[0]), &(latencies[runs-1]));
+   max_latency  = *max_element(&(latencies[0]), &(latencies[num_bufs-1]));
    printf("ISAL-Decompress,%d,,%f,%ld,\n",
    size, 
    std::accumulate(latencies.begin()+discard_fst,
-      std::next(latencies.begin(),runs-discard_fst), 0) / (runs*1.0),
+      std::next(latencies.begin(),num_bufs-discard_fst), 0) / (num_bufs*1.0),
       max_latency); 
    int stateful_deviations=0;
-   for(int i=0; i<runs; i++){
+   for(int i=0; i<num_bufs; i++){
       if(do_flush){
-         flush_buf(output_buffers[i%num_bufs], size);
-         flush_buf(input_buffers[i%num_bufs], size);
+         flush_buf(output_buffers[i], size);
+         flush_buf(input_buffers[i], size);
       }
       
       uint64_t start = micros();
       stream.total_out = 0;
       for(int j=0; j<iterations_per_run; j++){
-         stream.next_in = (uint8_t *)input_buffers[i%num_bufs];
+         stream.next_in = (uint8_t *)input_buffers[i];
          stream.avail_in = size;
-         stream.next_out = (uint8_t *)output_buffers[i%num_bufs];
+         stream.next_out = (uint8_t *)output_buffers[i];
          stream.avail_out = size;
          stream.end_of_stream = 0;
          stream.flush = SYNC_FLUSH;
@@ -190,12 +179,12 @@ int main(int argc, char **argv) {
       }
       uint64_t end = micros();
       // printf("latency,%ld\n", end-start);
-      if(compare_buffers(output_buffers[i%num_bufs], backup_buffers[i%num_bufs], stream.total_out) != -1){
+      if(compare_buffers(output_buffers[i], backup_buffers[i], stream.total_out) != -1){
          stateful_deviations+=1;
       }
       latencies[i] = (end - start)/iterations_per_run;      
    }
-   max_latency = *max_element(&(latencies[0]), &(latencies[runs-1]));
+   max_latency = *max_element(&(latencies[0]), &(latencies[num_bufs-1]));
    double avg_ratio = std::accumulate(&compressed_sizes[0],&compressed_sizes[num_bufs-1],0)/(num_bufs * size * 1.0);
    printf("ISAL-Compress,%d,%f,%f,%ld,%d\n",
       size,
@@ -203,8 +192,8 @@ int main(int argc, char **argv) {
       avg_ratio,
       std::accumulate(latencies.begin()+discard_fst, 
          std::next(latencies.begin(),
-            runs-discard_fst), 0)
-      / (runs*1.0),
+            num_bufs-discard_fst), 0)
+      / (num_bufs*1.0),
       max_latency,
       stateful_deviations);  
 
@@ -226,7 +215,7 @@ int main(int argc, char **argv) {
    //      return ret;
 
    // latencies.clear();
-   // for(int i=0; i<runs; i++){
+   // for(int i=0; i<num_bufs; i++){
    //    if(do_flush){
    //       flush_buf(output_buffer, sizeof output_buffer);
    //       flush_buf(input_buffer, sizeof input_buffer);
@@ -246,12 +235,12 @@ int main(int argc, char **argv) {
    //    printf("latency,%ld\n", end-start);  
    // }
 
-   // max_latency = *max_element(&(latencies[0]), &(latencies[runs-1]));
+   // max_latency = *max_element(&(latencies[0]), &(latencies[num_bufs-1]));
    // printf("Zlib-Compress,%d,%f,%f,%ld\n",
    // size,
    // size*1.0/strm.total_out,
    // std::accumulate(latencies.begin()+discard_fst,
-   //    std::next(latencies.begin(),runs-discard_fst), 0) / (runs*1.0),
+   //    std::next(latencies.begin(),num_bufs-discard_fst), 0) / (num_bufs*1.0),
    // max_latency);
 
    
