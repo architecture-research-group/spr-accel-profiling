047941445d1916a3ee288e1762ad07f2609a8167
Neel Patel <n869p538@sapphire.ittc.ku.edu>
2024-03-06 09:41:31 -0600

    avg ratio

--- cpu_compression_latency/harness-isal_zlib_decompress.cpp
+++ cpu_compression_latency/harness-isal_zlib_decompress.cpp
@@ -10,7 +10,7 @@
 #include "igzip_lib.h"
 #include <zlib.h>
 
-#define CALGARY "/lib/firmware/calgary"
+#define CALGARY "./calgary"
 
 // char input_buffer[64 * 1024 * 1024];
 // char output_buffer[64 * 1024 * 1024];
@@ -20,6 +20,12 @@ char level_buffer[64 * 1024 * 1024];
 using namespace std;
 uint64_t nanos() {
    return std::chrono::duration_cast< ::std::chrono::nanoseconds>(
+          std::chrono::steady_clock::now().time_since_epoch())
+          .count();
+}
+
+uint64_t micros() {
+   return std::chrono::duration_cast< ::std::chrono::microseconds>(
           std::chrono::steady_clock::now().time_since_epoch())
           .count();
 }
@@ -98,6 +104,8 @@ int main(int argc, char **argv) {
       output_buffers_2[i] = (char *)malloc(size);
    }
 
+   printf("Testing on %d buffers\n", num_bufs);
+
    /* DEFLATE */
    struct isal_zstream stream;
    isal_deflate_init(&stream);
@@ -113,21 +121,21 @@ int main(int argc, char **argv) {
       stream.flush = SYNC_FLUSH;
       isal_deflate(&stream);
       compressed_sizes[i] = stream.total_out;
-      // memcpy(backup_buffers[i], output_buffers[i], stream.total_out);
-      // printf("Compressed buffer %d to %d\n", i, stream.total_out);
+      memcpy(backup_buffers[i], output_buffers[i], stream.total_out);
+      // printf("Compressed buffer %d   to %d\n", i, stream.total_out);
       stream.total_out = 0;
    }
 
    int runs;
    int discard_fst = 10;
-   if(!do_flush){
+   // if(!do_flush){
       runs = 1000;
       discard_fst = 100;
-   }
-   else{
-      runs = 10;
-      discard_fst = 1;
-   }
+   // }
+   // else{
+      // runs = 10;
+      // discard_fst = 1;
+   // }
    iterations_per_run=1;
    uint64_t latency_avg, latency_min, max_latency;
    vector<uint64_t> latencies(runs);
@@ -136,10 +144,10 @@ int main(int argc, char **argv) {
    struct inflate_state state;
    for(int i=0; i<runs; i++){      
       if(do_flush){
-         flush_buf(output_buffers[i%num_bufs], size);
+         flush_buf(output_buffers[i%num_bufs], compressed_sizes[i%num_bufs]);
          flush_buf(output_buffers_2[i%num_bufs], size);
       }
-      uint64_t start = nanos();
+      uint64_t start = micros();
       for(int j=0; j<iterations_per_run; j++){
          state.next_in = (uint8_t*)output_buffers[i%num_bufs];
          state.avail_in = compressed_sizes[i%num_bufs];
@@ -147,25 +155,29 @@ int main(int argc, char **argv) {
          state.avail_out = size;
          isal_inflate(&state);
       }
-      uint64_t end = nanos();
+      uint64_t end = micros();
       // printf("latency,%ld\n", end-start);
-      assert(compare_buffers(output_buffers_2[i%num_bufs], input_buffers[i%num_bufs], size) == -1);
+      int mismatch;
+      if( (mismatch = compare_buffers(output_buffers_2[i%num_bufs], input_buffers[i%num_bufs], size)) != -1){
+         printf("Error: Decompression mismatch buffer:%d index:%d \n", i%num_bufs, mismatch );
+         // return -1;
+      }
       latencies[i]=((end - start)/iterations_per_run);      
    }
    max_latency  = *max_element(&(latencies[0]), &(latencies[runs-1]));
-   printf("ISAL-Decompress,%d,%f,%ld\n",
+   printf("ISAL-Decompress,%d,,%f,%ld,\n",
    size, 
    std::accumulate(latencies.begin()+discard_fst,
       std::next(latencies.begin(),runs-discard_fst), 0) / (runs*1.0),
       max_latency); 
-
+   int stateful_deviations=0;
    for(int i=0; i<runs; i++){
       if(do_flush){
-         flush_buf(output_buffers[i], size);
-         flush_buf(input_buffers[i], size);
+         flush_buf(output_buffers[i%num_bufs], size);
+         flush_buf(input_buffers[i%num_bufs], size);
       }
       
-      uint64_t start = nanos();
+      uint64_t start = micros();
       stream.total_out = 0;
       for(int j=0; j<iterations_per_run; j++){
          stream.next_in = (uint8_t *)input_buffers[i%num_bufs];
@@ -176,18 +188,25 @@ int main(int argc, char **argv) {
          stream.flush = SYNC_FLUSH;
          isal_deflate(&stream);
       }
-      uint64_t end = nanos();
-      printf("latency,%ld\n", end-start);
-      // assert(compare_buffers(output_buffers[i%num_bufs], backup_buffers[i%num_bufs], stream.total_out) == -1);
+      uint64_t end = micros();
+      // printf("latency,%ld\n", end-start);
+      if(compare_buffers(output_buffers[i%num_bufs], backup_buffers[i%num_bufs], stream.total_out) != -1){
+         stateful_deviations+=1;
+      }
       latencies[i] = (end - start)/iterations_per_run;      
    }
    max_latency = *max_element(&(latencies[0]), &(latencies[runs-1]));
-   printf("ISAL-Compress,%d,%f,%f,%ld\n",
-   size,
-   size*1.0/stream.total_out,
-   std::accumulate(latencies.begin()+discard_fst,
-      std::next(latencies.begin(),runs-discard_fst), 0) / (runs*1.0),
-      max_latency);  
+   double avg_ratio = std::accumulate(&compressed_sizes[0],&compressed_sizes[num_bufs-1],0)/(num_bufs * size * 1.0);
+   printf("ISAL-Compress,%d,%f,%f,%ld,%d\n",
+      size,
+      // size*1.0/stream.total_out,
+      avg_ratio,
+      std::accumulate(latencies.begin()+discard_fst, 
+         std::next(latencies.begin(),
+            runs-discard_fst), 0)
+      / (runs*1.0),
+      max_latency,
+      stateful_deviations);  
 
    
 
@@ -213,7 +232,7 @@ int main(int argc, char **argv) {
    //       flush_buf(input_buffer, sizeof input_buffer);
    //       _mm_mfence();
    //    }
-   //    uint64_t start = nanos();
+   //    uint64_t start = micros();
    //    for(int j=0; j<iterations_per_run; j++){
    //       strm.avail_in = size;
    //       strm.next_in = (Bytef *)input_buffer;
@@ -221,7 +240,7 @@ int main(int argc, char **argv) {
    //       strm.avail_out = sizeof output_buffer;
    //       deflate(&strm, Z_FINISH);
    //    }
-   //    uint64_t end = nanos();
+   //    uint64_t end = micros();
    //    // assert(compare_buffers(output_buffer, backup_buffer, size) == -1);
    //    latencies[i] = ((end - start)/iterations_per_run);    
    //    printf("latency,%ld\n", end-start);  
