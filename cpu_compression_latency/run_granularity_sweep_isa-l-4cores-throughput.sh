938df03227fe44f7d2556dc0e97bdea174f7d342
Neel Patel <n869p538@sapphire.ittc.ku.edu>
2024-03-08 13:17:14 -0600

    wait for test completion in granularity script

--- cpu_compression_latency/run_granularity_sweep_isa-l-4cores-throughput.sh
+++ cpu_compression_latency/run_granularity_sweep_isa-l-4cores-throughput.sh
@@ -5,10 +5,12 @@ LZBENCH=$(pwd)/lzbench-isal/lzbench
 CALGARY=/lib/firmware/calgary
 [ -f "results_*" ] && echo "Remove your results" && exit -1
 for i in ${SIZES[@]}; do 
+	echo "(de)compressing block size $i ... "
   for j in ${CORES[@]}; do
     block=$(( 1 << ( $i - 10 ) ))
     taskset -c ${j} $LZBENCH -elibdeflate,3 -eisa-l,2 -o4 -b${block} $CALGARY >> results_${block}kbchunk  &
   done
+  wait
 done
 
 # rm -rf result_*_bw
