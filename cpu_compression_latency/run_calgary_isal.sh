46930be041997217311755d2c6a08e0d767d238d
Neel Patel <n869p538@sapphire.ittc.ku.edu>
2024-03-12 22:14:47 -0500

    upd sizes

--- cpu_compression_latency/run_calgary_isal.sh
+++ cpu_compression_latency/run_calgary_isal.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SIZES=( 1024 4096 65536 $((256 * 1024 )) $(( 1024 * 1024 )) )
+SIZES=( 1024 4096 16384  )
 n=1000
 taskset -c 20 make
 for i in ${SIZES[@]}; do taskset -c 40 ./isal_zlib_decompress ${i} ${n} ;  done | sort -k1 -g -k4 -t,
